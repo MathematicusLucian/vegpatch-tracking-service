2ba1cd63030252bc3dfd27fd45bf0a67c6442823
k1n4kut4 <recruitlukejones@gmail.com>
2024-01-17 03:13:41 +0000

    plant config and state tracking setup logic

--- board.js
+++ board.js
@@ -1,5 +1,13 @@
 const { Board, Led } = require("johnny-five");
 const board = new Board();
+let plantsState = [];
+
+const PLANTS_CONFIG = [
+    { plantName: "Potatoes 1", soilSensor: "a7", dryState: "b0", wetState: "b1" },
+    { plantName: "Potatoes 2", soilSensor: "a7", dryState: "b0", wetState: "b1" },
+    { plantName: "Courgettes 1", soilSensor: "a7", dryState: "b0", wetState: "b1" },
+    { plantName: "Courgettes 2", soilSensor: "a7", dryState: "b0", wetState: "b1" }
+]
 
 const activated = () => {
     const led = new Led(13); //LED inserted directly into pin 13
@@ -7,6 +15,21 @@ const activated = () => {
     console.log("Vegpatch System Running!");
 }
 
+const addPlantState = (plantConfig) => {
+    plantsState.push = [
+        { 
+            name: plantConfig.plantName, 
+            soilState: five.Sensor(plantConfig.soilSensor), 
+            wetState: new five.Led(plantConfig.wetSensor), 
+            dryState: new five.Led(plantConfig.drySensor), 
+            bothMoistureStates: new five.Led([plantConfig.dryState, plantConfig.wetState]) 
+        }
+    ];
+}
+
 board.on("ready", () => {
     activated();
+    PLANTS_CONFIG.forEach(PLANT => {
+        addPlantState(PLANT);
+    });
 });
